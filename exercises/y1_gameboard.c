99a4a413396b3ffe4cc8e8dcf9a7c37889c28758
Andreas Würzer <awuerzer@stud.hs-heilbronn.de>
2024-11-20 13:35:28 +0100

    finish y1

--- exercises/y1_gameboard.c
+++ exercises/y1_gameboard.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void init_board(char board[][100], int height, int width, char defaultChar, char borderChar) {
     // Initialize the board with a default character 
@@ -19,6 +21,23 @@ void init_board(char board[][100], int height, int width, char defaultChar, char
     }
 }
 
+void add_single_block(char board[][100], int x, int y, int size, char blockChar, int height, int width) {
+    // x and y are the starting coords, loop through size and add char
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (y+i > height || x+j > width)
+            {
+                break;
+            }
+            
+            board[y+i][x+j] = blockChar;
+        }
+    }
+}
+
+
 void show_game_board(char board[][100], int height, int width) {
     // Print the board
     for (int i = 0; i < height; i++) {
@@ -36,7 +55,19 @@ int main() {
     char borderChar = '#';
     char board[100][100];
 
-    init_board(board, height, width, defaultChar, borderChar);    
+    // init rand()
+    srand(time(NULL));
+
+    init_board(board, height, width, defaultChar, borderChar); 
+
+    for (int i = 0; i < 20; i++)
+    {
+        int size = rand() % 4 + 3;
+        int y = rand() % height;
+        int x = rand() % width;
+
+        add_single_block(board, x, y, size, borderChar, height, width);
+    }
 
     show_game_board(board, height, width);
 
